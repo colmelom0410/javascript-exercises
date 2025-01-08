a130bdd4a10cf52263076b0f39218b0461688155
Shalom S. De Veyra <191439611+colmelom0410@users.noreply.github.com>
2025-01-08 11:59:53 +0800

    Add function for Celcius and Fahrenheit conversion
    Javascript exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let Cel = (temp - 32)*(5/9);
+  return Math.round(Cel*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let Far = temp*(9/5)+32;
+  return Math.round(Far*10)/10;
 };
 
 // Do not edit below this line
