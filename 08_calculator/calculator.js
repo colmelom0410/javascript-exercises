76eac570c7258d4c0b2011b6519e2e01e0769aa6
Shalom S. De Veyra <191439611+colmelom0410@users.noreply.github.com>
2025-01-13 08:48:44 +0800

    Do calculator functions and test the results

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, number) => (sum+number),0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product,number) => (product*number),1);
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  // return num**exponent;
+	return Math.pow(num,exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1){
+    return 1;
+  }
+  for(let i = num-1; i>=1; i--){
+    num*=i;
+  }
+  return num;
 };
 
 // Do not edit below this line
