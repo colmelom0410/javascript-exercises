0da75a421b7eba966c85c8bba8899b8cf1c66b8b
Shalom S. De Veyra <191439611+colmelom0410@users.noreply.github.com>
2025-01-14 14:22:46 +0800

    Use .map() to get book titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
 const getTheTitles = function() {
-
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ]
+    const bookTitle = books.map(book => (book.title));
+    return bookTitle;
 };
 
 // Do not edit below this line
