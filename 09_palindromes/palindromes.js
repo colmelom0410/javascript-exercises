5b1f7a4f368ffd29ad24893ca9a2f2e30524d1f9
Shalom S. De Veyra <191439611+colmelom0410@users.noreply.github.com>
2025-01-14 11:53:32 +0800

    Refactor palindromes function to reverse starting

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    // text = text.replace(/[?!, .]/g,"").toLowerCase();
+    // let newText ="";
+    // for (let i = text.length - 1; i>=0; i--){
+    //     newText += text[i];
+    // }
+    // if (text === newText){
+    //     return true;
+    // }
+    // return false;
+
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    const oldText = text.toLowerCase().split("").filter(char => (alphanumerical.includes(char))).join("");
+    const newText = oldText.split("").reverse().join("");
+
+    return oldText === newText;
+
 
 };
 
