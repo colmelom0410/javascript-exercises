24f5887e714472b5e349913d0beb3c2ab34cda98
Shalom S. De Veyra <191439611+colmelom0410@users.noreply.github.com>
2025-01-14 15:40:28 +0800

    Add function to find the oldest in people array
    -used .filter() to get the person with undefined yearOfDeath.
    -used .reduce() to get the oldest person.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    const oldest = people
+        // filter the one with undefined year of death and set it to current date
+        .filter(person => {
+            if(typeof person.yearOfDeath === "undefined"){
+                person.yearOfDeath = new Date().getFullYear()}
+            return true;
+            })
+        //sort from oldest to youngest
+        // .sort((a,b) => (a.yearOfDeath - a.yearOfBirth)>(b.yearOfDeath - b.yearOfBirth)? -1:1)
+        .reduce((older, person) => (person.yearOfDeath - person.yearOfBirth < 
+            older.yearOfDeath - older.yearOfBirth? older:older=person),"");
+        //return the name only
 
+    // return oldest[0];
+    return oldest;
 };
 
 // Do not edit below this line
