097f4701c9eaef17ebb48f96352427ca03efc7db
Shalom S. De Veyra <191439611+colmelom0410@users.noreply.github.com>
2025-01-14 14:25:45 +0800

    Delete unnecessary variable

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,14 +1,4 @@
-const getTheTitles = function() {
-    const books = [
-        {
-          title: 'Book',
-          author: 'Name'
-        },
-        {
-          title: 'Book2',
-          author: 'Name2'
-        }
-      ]
+const getTheTitles = function(books) {
     const bookTitle = books.map(book => (book.title));
     return bookTitle;
 };
